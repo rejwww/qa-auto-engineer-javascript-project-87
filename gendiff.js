0fa02dd8f66545a3b8b5a54ed6c53391088d35d9
Angelina Kuptsova <linakuptsova17@gmail.com>
2026-06-29 13:03:34 +0400

    add read file

--- gendiff.js
+++ gendiff.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import genDiff from './src/index.js'
+
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0')
+  .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .action((file1, file2) => {
+    const options = program.opts()
+    let result = genDiff(file1, file2, options.format)
+    console.log(result)
+  });
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
-const genDiff = (file1,file2)  =>{return ''}
+import parser from './parser.js'
+
+const genDiff = (file1,file2)  =>{
+  const parserFile1 = parser(file1)
+  const parserFile2 = parser(file2)
+
+    return parserFile1 }
 
 export default genDiff
